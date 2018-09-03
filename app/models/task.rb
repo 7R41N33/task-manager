5ee1df4763c96144b0ee0a7b544845a771ba72cd
7R41N33 <chepenkoanton2011@yandex.ru>
2018-09-03 18:00:49 +0400

    task state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,33 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine :initial => :new do
+    event :develop do
+      transition :new => :in_development,
+                 :in_qa => :in_development,
+                 :in_code_review => :in_development
+    end
+
+    event :archive do
+      transition :new => :archived,
+                 :released => :archived
+    end
+
+    event :qa do
+      transition :in_development => :in_qa
+    end
+
+    event :review do
+      transition :in_qa => :in_code_review
+    end
+
+    event :release_ready do
+      transition :in_code_review => :ready_for_release
+    end
+
+    event :release do
+      transition :ready_for_release => :released
+    end
+  end
 end
