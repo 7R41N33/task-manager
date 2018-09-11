14cf2f5cec832ecf1a332902e62b1a543bc4d691
7R41N33 <chepenkoanton2011@yandex.ru>
2018-09-11 17:52:04 +0400

    api

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :task do
-    name 'MyString'
-    description 'MyText'
-    author_id 1
-    assignee_id 1
-    state 'MyString'
-    expired_at '2018-09-03'
+    name { generate :string }
+    description { generate :string }
+    author_id { create :manager }
+    assignee_id { create :developer }
   end
 end
